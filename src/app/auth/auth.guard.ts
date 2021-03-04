b2406950cd98db2e2cdbd53f0dcbc7ec6a150cf7
josipsusic <susic@uhp-digital.com>
2021-03-04 07:45:18 +0100

    Implement auth guard

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { take, skipWhile, tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return true;
+  }
+  // If we return boolean or observable from canLoad, it must
+  // eventually be marked as complete, otherwise Angular
+  // assumes that we're still waiting for some value to come out
+  // to decide whether or not it's gonna allow navigation
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.signedin$.pipe(
+      skipWhile((value) => value === null),
+      take(1),
+      tap((authenticated) => {
+        if (!authenticated) {
+          this.router.navigateByUrl('/');
+        }
+      })
+    );
+  }
+}
